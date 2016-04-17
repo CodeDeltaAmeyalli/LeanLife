831674fa6e65a8824757234ebc5bb368e6393562
Escuela Ameyalli <ea@imac-5.local>
2016-04-17 12:42:07 -0500

    PickerView is not finished yet

--- LeanLife/SecondViewController.swift
+++ LeanLife/SecondViewController.swift
@@ -8,16 +8,45 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var pickerActivity: UIPickerView!
+    var pickerActivityData: [String] = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Connect data:
+        
+        self.pickerActivity.delegate = self
+        self.pickerActivity.dataSource = self
+        
+        //Insert objects in the array.
+        pickerActivityData = ["Sedentary - little or no exercise", "Lightly Active - 1-3 times/week", "Moderately Active - 3-5 times/week", "Very Active - 6-7 times/week", "Extra Active"
+        ]
+        
+    
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // The number of columns of data
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerActivityData.count
+    }
+    
+    // The data to return for the row and component (column) that's being passed in
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerActivityData[row]
     }
 
 
