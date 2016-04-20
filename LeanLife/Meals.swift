33bda5ccac38415a15609e0c10cd63b90a1bef1b
Adriana <adriana.dp@ameyalli.edu.mx>
2016-04-20 10:09:39 -0500

    classes created

---
+++ LeanLife/Meals.swift
@@ -0,0 +1,40 @@
+//
+//  Meals.swift
+//  LeanLife
+//
+//  Created by Escuela Ameyalli on 4/20/16.
+//  Copyright © 2016 Escuela Ameyalli. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    // MARK: Properties 
+    var photo: UIImage?
+    var name: String
+    var date: NSDate
+    var quantity: Double
+    var proteins: Int
+    var carbohydrates: Int
+    var fat: Int
+    var calories: Int
+    
+    //MARK: Initialization 
+    init?(photo: UIImage, name: String, date:NSDate, quantity: Double, proteins: Int, carbohydrates: Int, fat: Int, calories: Int){
+      //Initialize stored properties 
+        self.photo = photo
+        self.name = name
+        self.date = date
+        self.quantity = quantity
+        self.proteins = proteins
+        self.carbohydrates = carbohydrates
+        self.fat = fat
+        self.calories = calories
+    }
+   
+    
+    
+    
+   
+    
+}
