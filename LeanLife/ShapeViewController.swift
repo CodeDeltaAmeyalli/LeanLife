0d43e52cbf4e043e1f4e290544279240de99eeda
Escuela Ameyalli <ea@imac-55.local>
2016-04-21 18:31:55 -0500

    Gords listas

--- LeanLife/ShapeViewController.swift
+++ LeanLife/ShapeViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ShapeViewController: UIViewController {
 
+    @IBOutlet weak var shapeSelectedLabel: UILabel!
+    @IBOutlet weak var shape1: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -31,5 +34,38 @@ class ShapeViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    @IBAction func shape1(sender: UITapGestureRecognizer) {
+        shapeSelectedLabel.text = "Selected boy shape : 1"
+        shape1.highlighted = true
+    }
+    @IBAction func shape2(sender: UITapGestureRecognizer) {
+        shapeSelectedLabel.text = "Selected boy shape : 2"
+    }
+    
+    @IBAction func shape3(sender: UITapGestureRecognizer) {
+         shapeSelectedLabel.text = "Selected boy shape : 3"
+    }
+    
+    @IBAction func shape4(sender: AnyObject) {
+        shapeSelectedLabel.text = "Selected boy shape : 4"
+    }
+    @IBAction func shape5(sender: AnyObject) {
+        shapeSelectedLabel.text = "Selected boy shape : 5"
+    }
 
+    @IBAction func shape6(sender: AnyObject) {
+        shapeSelectedLabel.text = "Selected boy shape : 6"
+    }
+    @IBAction func shape7(sender: AnyObject) {
+        shapeSelectedLabel.text = "Selected boy shape : 7"
+    }
+    @IBAction func shape8(sender: AnyObject) {
+        shapeSelectedLabel.text = "Selected boy shape : 8"
+    }
+    @IBAction func shape9(sender: AnyObject) {
+        shapeSelectedLabel.text = "Selected boy shape : 9"
+    }
+    
+ 
 }
